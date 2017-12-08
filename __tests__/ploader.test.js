e0f00ab690a606c405fbb618be9ca4a9a4499ee3
dzmitry.panamarenka <dzmitry.panamarenka@gmail.com>
2017-12-08 04:13:33 +0300

    #ploader fixed mistakes after step 1

--- __tests__/ploader.test.js
+++ __tests__/ploader.test.js
@@ -4,42 +4,42 @@ import path from 'path';
 import nock from 'nock';
 import axios from 'axios';
 import httpAdapter from 'axios/lib/adapters/http';
-import loader from '../src';
-
-const host = 'https://hexlet.io';
-const { sep } = path;
-const dir = fs.mkdtempSync(path.join(os.tmpdir(), sep, 'ploader-'));
-const address = `${host}/courses`;
-
-axios.defaults.host = host;
-axios.defaults.adapter = httpAdapter;
+import loadPage from '../src';
 
 describe('ploader', () => {
-  it('get #1 page: success', (done) => {
+  let dir;
+  const { sep } = path;
+  const host = 'https://hexlet.io';
+  const address = `${host}/courses`;
+  axios.defaults.adapter = httpAdapter;
+
+  beforeEach(() => {
+    dir = fs.mkdtempSync(path.join(os.tmpdir(), sep, 'ploader-'));
+  });
+
+  it('get #1 page: success', () => {
     const status = 200;
     nock(host).get('/courses').reply(status);
 
-    loader(address, dir).then((response) => {
+    return loadPage(address, dir).then((response) => {
       expect(response.status).toBe(status);
-      done();
-    }).catch(done);
+    });
   });
 
-  it('get #1 page: failed', (done) => {
+  it('get #1 page: failed', () => {
     const status = 404;
     nock(host).get('/courses/failed').reply(status);
+    expect.assertions(1);
 
-    loader(`${address}/failed`, dir).then((response) => {
-      fail(response.status);
-    }).catch((err) => {
+    return loadPage(`${address}/failed`, dir).catch((err) => {
       expect(err).not.toBeNull();
-      done();
     });
   });
 
   it('get #1 page: body', () => {
     nock(host).get('/courses').reply(200);
-    return loader(address, dir).then(() => {
+
+    return loadPage(address, dir).then(() => {
       fs.open(`${dir}/hexlet-io-courses.html`, 'r', (err, data) => {
         expect(data).toBeTruthy();
       });
--- src/index.js
+++ src/index.js
@@ -5,26 +5,18 @@ import axios from 'axios';
 // import _ from 'lodash';
 // import path from 'path';
 
-const loader = (address, folder) => {
+const loadPage = (address, folder) => {
   const { hostname, pathname } = url.parse(address);
   const fileName = `${hostname.split('.').join('-')}${pathname.split('/').join('-')}.html`;
-
-  return new Promise((resolve, reject) => {
-    axios.get(address)
+  const filePath = `${folder}/${fileName}`;
+    return axios.get(address)
       .then((res) => {
-        fs.writeFile(`${folder}/${fileName}`, res.data)
-          .then((err) => {
-            if (err) {
-              throw err;
-            }
-            return resolve({
-              data: res.data,
-              status: res.status,
-            });
-          });
+        fs.writeFile(filePath, res.data);
+        return {
+          data: res.data,
+          status: res.status,
+        };
       })
-      .catch(err => reject(err));
-  });
 };
 
-export default loader;
+export default loadPage;
