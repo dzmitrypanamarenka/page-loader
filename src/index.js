aa6b0c37a675ce01ba4ae835e1bb32d8e0b98d12
dzmitry.panamarenka <dzmitry.panamarenka@gmail.com>
2017-12-09 22:24:43 +0300

    #ploader added debugger

--- src/index.js
+++ src/index.js
@@ -2,15 +2,22 @@ import fs from 'mz/fs';
 import url from 'url';
 import axios from 'axios';
 import cheerio from 'cheerio';
+import debug from 'debug';
 import { getLocalPath, getLinkPath } from './localPath';
+
+const pageDebug = debug('ploader:page');
+pageDebug('start loading your page');
 
 const loadPage = (address, folder) => {
   const indexHtml = `${folder}/${getLocalPath(address, true)}`;
   const localPath = `${folder}/${getLocalPath(address)}`;
+  pageDebug('sending request');
   return axios(address)
     .then((res) => {
+      pageDebug('receiving html');
       const $ = cheerio.load(res.data, { decodeEntities: false });
       const resources = [];
+      pageDebug('selecting assets');
       $('script[src]').add('link[href]').add('img[src]').each((i, el) => {
         const attribute = $(el).prop('name') === 'link' ? 'href' : 'src';
         resources.push($(el).attr(attribute));
@@ -18,15 +25,17 @@ const loadPage = (address, folder) => {
         const newAttr = url.format({ host: localPath, pathname: localUrl });
         $(el).attr(attribute, newAttr);
       });
-
+      pageDebug('updating html with local assets');
       fs.writeFile(indexHtml, $.html());
 
+      pageDebug('creating directory for local assets');
       fs.open(localPath, 'r', (err) => {
         if (err) {
           fs.mkdir(localPath);
         }
       });
 
+      pageDebug('downloading assets');
       return Promise.all(resources.map((el) => axios({
         method: 'get',
         url: el,
@@ -34,6 +43,7 @@ const loadPage = (address, folder) => {
       })));
     })
     .then((resp) => {
+      pageDebug('writing assets');
       return Promise.all(resp.map((el) => {
         const file = getLinkPath(el.request.path);
         return el.data.pipe(fs.createWriteStream(`${localPath}${file}`));
